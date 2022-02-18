15fe4fcf0bfff62b1b749f0eff384baecf2734e0
aljoromer <aljoromer@gmail.com>
2022-02-17 21:31:51 -0500

    added coursenum method to filter by course number

--- schedule.py
+++ schedule.py
@@ -55,6 +55,9 @@ class Schedule():
         ''' filters the courses by number of students in waiting list '''
         return Schedule([course for course in self.courses if int(course['waiting']) <= int(number)])
 
+    def coursenum(self, num):
+        '''filters the courses by course number'''
+        return Schedule([course for course in self.courses if num in course['coursenum']])    
 
     def sort(self,field):
         ''' subject filters the courses by field '''
